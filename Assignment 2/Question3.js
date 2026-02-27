730f9dfda83c1039f3c3c269e8fa213db96ec069
ShraddhaDhotre <dhotreshraddha08@gmail.com>
2026-02-27 14:06:15 +0530

    findTax using switch case

---
+++ Assignment 2/Question3.js
@@ -0,0 +1,28 @@
+function findTax(salary) {
+  let taxRate;
+
+  switch (true) {
+    case salary > 0 && salary <= 500000:
+      taxRate = 0;
+      break;
+    case salary > 500000 && salary <= 1000000:
+      taxRate = 0.10;
+      break;
+    case salary > 1000000 && salary <= 1500000:
+      taxRate = 0.20;
+      break;
+    case salary > 1500000:
+      taxRate = 0.30;
+      break;
+    default:
+      return "Invalid salary";
+  }
+
+  return salary * taxRate;
+}
+
+// Test
+console.log(findTax(400000));   
+console.log(findTax(750000));   
+console.log(findTax(1200000));  
+console.log(findTax(2000000));  
